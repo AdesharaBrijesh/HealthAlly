52e8453d25783a8770e52fefba854d7ad34c31b8
AdesharaBrijesh <adesharabrijesh8@gmail.com>
2024-11-06 23:53:45 +0530

    Refactor MONGO_URI in config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # config.py
 
-MONGO_URI = "mongodb+srv://mrhashtag292:qLeJHyZFTClgHixL@cluster.wooif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
+# MONGO_URI = "mongodb+srv://mrhashtag292:qLeJHyZFTClgHixL@cluster.wooif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
+MONGO_URI = "mongodb+srv://mrhashtag292:qLeJHyZFTClgHixL@cluster0.mongodb.net/test?retryWrites=true&w=majority"
